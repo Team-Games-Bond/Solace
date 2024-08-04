637295daeb7665415bdc2c602e60655ac8579a6f
Madeline Whitmore <madeline-whitmore@protonmail.com>
2024-08-05 11:47:53 +1200

    Add Interaction Signals and A Simple Way To Recieve Them

---
+++ Scripts/ExampleInteraction.cs
@@ -0,0 +1,26 @@
+using Godot;
+
+// Needs tool annotation for warnings to work properly
+[Tool]
+public partial class ExampleInteraction : AbstractInteraction
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Use for code you don't want running in editor
+		if (!Engine.IsEditorHint()){
+			// Call _Ready from the base class if you override it
+			// Also call _GetConfigurationWarnings if you override it
+			base._Ready();
+		}
+	}
+    public override void _Process(double delta)
+    {
+        GD.Print("afjdsklfgjdaslk");
+    }
+
+    public override void Interact()
+    {
+		GD.PrintRich("[shake rate=20.0 level=5 connected=1][rainbow]Player Interacted[/rainbow][/shake]");
+    }
+}
