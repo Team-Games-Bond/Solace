0bff5c0119381d4cd5fb5ccaf6ab98fdc2d81f61
Madeline Whitmore <madeline-whitmore@protonmail.com>
2024-08-07 10:54:02 +1200

    Camera Interactions

---
+++ Scripts/CameraInteraction.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+// Needs tool annotation for warnings to work properly
+[Tool]
+public partial class CameraInteraction : Camera3D
+{
+	private CameraController controller;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		if (!Engine.IsEditorHint()){
+			// Makes the assumption that there is only one camera controller in a scene
+			controller = (CameraController)GetTree().GetFirstNodeInGroup("Character Controllers");
+			GetParent<Interactable>().Interaction += Interact;
+		}
+	}
+	public override string[] _GetConfigurationWarnings()
+    {
+		List<string> warnings = new List<string>();
+		try{
+			if (GetParent<Interactable>() == null){
+				warnings.Add(string.Format("{0} needs to be a child of an Area3D with an Interactable Script Attatched", GetType().Name ));
+			}
+		} catch (InvalidCastException)
+        {
+			warnings.Add(string.Format("{0} needs to be a child of an Area3D with an Interactable Script Attatched", GetType().Name ));
+		}
+        return warnings.ToArray();
+    }
+    public override void _Process(double delta)
+    {
+    }
+
+    public void Interact()
+    {
+		controller.ChangeCamera(this);
+    }
+}
