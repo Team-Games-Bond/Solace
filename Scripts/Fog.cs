5a6a5c5277f0908b22e830951ac9b66048291177
Madeline Whitmore <madeline-whitmore@proton.me>
2024-09-03 17:16:41 +1200

    Fog Improvements Add Custom Procedural Mesh

--- Scripts/Fog.cs
+++ Scripts/Fog.cs
@@ -1,32 +1,68 @@
 using Godot;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
+using Vector2 = Godot.Vector2;
+using Vector3 = Godot.Vector3;
 
+[Tool]
 public partial class Fog : MeshInstance3D
 {
+	[ExportCategory("Fog Offset")]
 	[Export] public Texture2D PrimaryTexture;
 	[Export] public Texture2D SecondaryTexture;
-	[Export]public Timer TransitionTimer;
-	public ShaderMaterial FogMat;
+	[Export] public Timer TransitionTimer;
 	public int map_width, map_height;
 
 	private byte[] _primaryImage;
 	private Task _combineTask; 
+
+	[ExportCategory("Fog Mesh")]
+	private Vector2I _subdivisions = new Vector2I(3, 3);
+	private Vector2 _planeScale = new Vector2(1,1); 
+	[Export] public Vector2I Subdivisions {
+		get=>_subdivisions;
+		set
+		{
+			_subdivisions = new Vector2I(Math.Max(value.X, 1), Math.Max(value.Y, 1));
+			Refresh();
+		}
+	}
+	[Export] public Vector2 PlaneScale { 
+		get=>_planeScale;
+		set
+		{
+			_planeScale = new Vector2(Math.Max(value.X, 0f), Math.Max(value.Y, 0f));
+			Refresh();
+		}
+	}
+	private ShaderMaterial _fogMaterial = GD.Load<ShaderMaterial>("res://Materials/FogMaterial.tres");
+	[Export] public ShaderMaterial FogMaterial {
+		get => _fogMaterial;
+		set
+		{
+			_fogMaterial = value;
+			Refresh();
+		}
+	}
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		FogMat = (ShaderMaterial)GetActiveMaterial(0);
-		var image = PrimaryTexture.GetImage();
-		map_width = image.GetWidth();
-		map_height = image.GetHeight();
-		FogMat.SetShaderParameter("PRIMARY_DEPTH", PrimaryTexture);
-		//FogMat.SetShaderParameter("SECONDARY_DEPTH", SecondaryTexture);
-		image.Convert(Image.Format.R8);
-		_primaryImage = image.GetData();
-		TransitionTimer.Timeout+=endTransition;
+		if (!Engine.IsEditorHint())
+		{
+			var image = PrimaryTexture.GetImage();
+			map_width = image.GetWidth();
+			map_height = image.GetHeight();
+			_fogMaterial.SetShaderParameter("PRIMARY_DEPTH", PrimaryTexture);
+			//FogMat.SetShaderParameter("SECONDARY_DEPTH", SecondaryTexture);
+			image.Convert(Image.Format.R8);
+			_primaryImage = image.GetData();
+			TransitionTimer.Timeout+=endTransition;
+		}
 	}
 	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,7 +79,7 @@ public partial class Fog : MeshInstance3D
 			endTransition();
 		}
 		TransitionTimer.Start();
-		FogMat.SetShaderParameter("SECONDARY_DEPTH", image);
+		_fogMaterial.SetShaderParameter("SECONDARY_DEPTH", image);
 		_combineTask = combine(_primaryImage, image);
 	}
 	public async void endTransition(){
@@ -99,5 +135,59 @@ public partial class Fog : MeshInstance3D
 		if (@event is InputEventKey eventKey)
 			if (eventKey.Pressed && eventKey.Keycode == Key.M)
 				Recede(SecondaryTexture);
+	}
+	public void Refresh(){
+		if (Engine.IsEditorHint()) MeshGen();
+	}
+	public async void MeshGen(){
+		var verticesTask = new Task<Vector3[]>(()=>{
+			var vertices = new Vector3[(Subdivisions.X+2)*(Subdivisions.Y+2)];
+			for (int y = 0; y<Subdivisions.Y+2; y++){
+				for (int x = 0; x<Subdivisions.X+2; x++){
+					int i = y*(Subdivisions.X+2)+x;
+					vertices[i] = new Vector3(((x+((y%2==1)?0.25f:-0.25f))/(Subdivisions.X+1)-0.5f)*PlaneScale.X, 0, (((float)y)/((float)(Subdivisions.Y+1))-0.5f)*PlaneScale.Y);
+				}
+			}
+			return vertices;
+		});
+		verticesTask.Start();
+		var indicesTask = new Task<int[]>(()=>{
+			var indices = new int[(Subdivisions.X+1)*(Subdivisions.Y+1)*2*3];
+			for (int y = 0; y<Subdivisions.Y+1; y++){
+				for (int x = 0; x<Subdivisions.X+1; x++){
+					int i = (y*(Subdivisions.X+1)+x)*6;
+					if(y%2==0)
+					{	
+						indices[i]=y*(Subdivisions.X+2)+x;
+						indices[i+1]=y*(Subdivisions.X+2)+x+1;
+						indices[i+2]=(y+1)*(Subdivisions.X+2)+x;
+						indices[i+3]=(y+1)*(Subdivisions.X+2)+x;
+						indices[i+4]=y*(Subdivisions.X+2)+x+1;
+						indices[i+5]=(y+1)*(Subdivisions.X+2)+x+1;
+					} 
+					else 
+					{
+						indices[i]=y*(Subdivisions.X+2)+x;
+						indices[i+1]=(y+1)*(Subdivisions.X+2)+x+1;
+						indices[i+2]=(y+1)*(Subdivisions.X+2)+x;
+						indices[i+3]=y*(Subdivisions.X+2)+x;
+						indices[i+4]=y*(Subdivisions.X+2)+x+1;
+						indices[i+5]=(y+1)*(Subdivisions.X+2)+x+1;
+					}
+				}
+			}
+			return indices;
+		});
+		indicesTask.Start();
+		var arrayMesh = new ArrayMesh();
+		Mesh=arrayMesh;
+		var arrays = new Godot.Collections.Array();
+		arrays.Resize((int)Mesh.ArrayType.Max);
+		await verticesTask;
+		await indicesTask;
+		arrays[(int)Mesh.ArrayType.Vertex] = verticesTask.Result;
+		arrays[(int)Mesh.ArrayType.Index] = indicesTask.Result;
+		arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, arrays);
+		arrayMesh.SurfaceSetMaterial(0, FogMaterial);
 	}
 }
