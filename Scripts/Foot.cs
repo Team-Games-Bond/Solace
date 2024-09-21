78d8c19c5bcc49be90c0bee687ba523671e2f4f0
Madeline Whitmore <madeline-whitmore@protonmail.com>
2024-09-21 13:53:31 +1200

    Reliability Tweaks

--- Scripts/Foot.cs
+++ Scripts/Foot.cs
@@ -1,11 +1,10 @@
 using Godot;
-using Godot.NativeInterop;
-using System;
 
 public partial class Foot : Node3D
 {
 	[Export] public float StepSpeed = 15;
 	[Export] public PackedScene footprint = ResourceLoader.Load<PackedScene>("res://Scenes/FootPrint.tscn");
+	Leg attachedLeg;
 	public Vector3 targetPos;
 	Vector3 startPos;
 
@@ -27,6 +26,7 @@ public partial class Foot : Node3D
 			if (_t>=1){
 				_t = 1;
 				moving = false;
+				attachedLeg.coordinator.completeStep(attachedLeg);
 			}
 			GlobalPosition = MadsHermite(startPos, targetPos, Vector3.Up*d, Vector3.Up*d, (float)_t);
 		}
@@ -35,7 +35,9 @@ public partial class Foot : Node3D
 	Vector3 MadsHermite(Vector3 p1, Vector3 p2, Vector3 n1, Vector3 n2, float t) {
 		return p1*((1+2*t)*(1-t)*(1-t))+n1*(t*(1-t)*(1-t))+p2*(t*t*(3-(2*t)))+n2*(t*t*(1-t));
 	}
-	
+	public void Setup(Leg leg){
+		attachedLeg = leg;
+	}
 	public void Step(Vector3 target){
 		if (!moving){
 			targetPos = target;
