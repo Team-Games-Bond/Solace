de99423925326468653c1ba0908c972af4e580e7
Melody Elwood <belwood4@gmail.com>
2024-09-26 10:19:05 +1200

    Fixed Fog
    - Added fog to main level and fixed it

--- Scripts/Fog.cs
+++ Scripts/Fog.cs
@@ -12,25 +12,30 @@ public partial class Fog : MeshInstance3D
 {
 	[ExportCategory("Fog Offset")]
 	[Export] public Texture2D InitialTexture;
+	[Export] public Sprite3D sprite;
 	[Export] public Timer TransitionTimer;
 	public int map_width, map_height;
 
 	private byte[] _primaryImage;
-	private Task _combineTask; 
+	private Task _combineTask;
 
 	[ExportCategory("Fog Mesh")]
 	private Vector2I _subdivisions = new Vector2I(3, 3);
-	private Vector2 _planeScale = new Vector2(1,1); 
-	[Export] public Vector2I Subdivisions {
-		get=>_subdivisions;
+	private Vector2 _planeScale = new Vector2(1, 1);
+	[Export]
+	public Vector2I Subdivisions
+	{
+		get => _subdivisions;
 		set
 		{
 			_subdivisions = new Vector2I(Math.Max(value.X, 1), Math.Max(value.Y, 1));
 			Refresh();
 		}
 	}
-	[Export] public Vector2 PlaneScale { 
-		get=>_planeScale;
+	[Export]
+	public Vector2 PlaneScale
+	{
+		get => _planeScale;
 		set
 		{
 			_planeScale = new Vector2(Math.Max(value.X, 0f), Math.Max(value.Y, 0f));
@@ -38,7 +43,9 @@ public partial class Fog : MeshInstance3D
 		}
 	}
 	private ShaderMaterial _fogMaterial = GD.Load<ShaderMaterial>("res://Materials/FogMaterial.tres");
-	[Export] public ShaderMaterial FogMaterial {
+	[Export]
+	public ShaderMaterial FogMaterial
+	{
 		get => _fogMaterial;
 		set
 		{
@@ -58,116 +65,163 @@ public partial class Fog : MeshInstance3D
 			_fogMaterial.SetShaderParameter("PRIMARY_DEPTH", InitialTexture);
 			image.Convert(Image.Format.R8);
 			_primaryImage = image.GetData();
-			TransitionTimer.Timeout+=endTransition;
+			TransitionTimer.Timeout += endTransition;
 		}
 	}
-	
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		if (!TransitionTimer.IsStopped())
 		{
-			SetInstanceShaderParameter("SECONDARY_STRENGTH", 1-(TransitionTimer.TimeLeft/TransitionTimer.WaitTime));
+			SetInstanceShaderParameter("SECONDARY_STRENGTH", 1 - (TransitionTimer.TimeLeft / TransitionTimer.WaitTime));
+			//GD.Print(TransitionTimer.TimeLeft/TransitionTimer.WaitTime);
 		}
 	}
 
 	public async void Recede(Texture2D image)
 	{
-		if(!TransitionTimer.IsStopped()){
+		//GD.Print("Receding");
+		if (!TransitionTimer.IsStopped())
+		{
 			endTransition();
 		}
 		TransitionTimer.Start();
 		_fogMaterial.SetShaderParameter("SECONDARY_DEPTH", image);
-		_combineTask = combine(_primaryImage, image);
+		//GD.Print(_primaryImage.Length);
+		_combineTask = combine(_primaryImage, image, sprite);
 	}
 
-	public async void endTransition(){
-		await _combineTask;
-		TransitionTimer.Stop();
-		new Task(()=>{
-			var img = ImageTexture.CreateFromImage(Image.CreateFromData(map_width, map_height, false, Image.Format.R8, _primaryImage));
-			SetInstanceShaderParameter("HEIGHT_TEXTURE_STRENGTH", 0);
-		});
-	}
-	
-	public static async Task combine(byte[] target, Texture2D from)
+	public async void endTransition()
 	{
-		Stopwatch sw = new Stopwatch();
+		//GD.Print("End");
+		await _combineTask;
+		//GD.Print(_primaryImage.Length);
+		//GD.Print("combineTask");
+		TransitionTimer.Stop();
+		//new Task(()=>{
+		//GD.Print("Newtaskstart");
+		Texture2D img = ImageTexture.CreateFromImage(Image.CreateFromData(map_width, map_height, false, Image.Format.R8, _primaryImage[0..65536]));
+		//_primaryImage = img.GetData();
+		_fogMaterial.SetShaderParameter("SECONDARY_STRENGTH", 0);
+		//SetInstanceShaderParameter("PRIMARY_DEPTH", img);
+		_fogMaterial.SetShaderParameter("PRIMARY_DEPTH", img);
+		//});
+	}
+
+	public static async Task combine(byte[] target, Texture2D from, Sprite3D sprite)
+	{
+		//GD.Print("combine running");
+		Stopwatch sw = new Stopwatch(); //Not used
 		sw.Start();
-		var fromTask = new Task<ushort[]>(()=> {
-			var fromData = from.GetImage().GetData();
-			return fromData.Select(r=>(ushort)r).ToArray();
-			} );
+		var fromTask = new Task<ushort[]>(() =>
+		{
+			var image = from.GetImage();
+			image.Convert(Image.Format.R8);
+			var fromData = image.GetData();
+			return fromData.Select(r => (ushort)r).ToArray();
+		});
 		fromTask.Start();
-		var targetTask = new Task<ushort[]>(()=> target.Select(r=>(ushort)r).ToArray());
+		var targetTask = new Task<ushort[]>(() => target.Select(r => (ushort)r).ToArray());
 		targetTask.Start();
-		
+
 		int length = target.Length;
 		int remaining = length % Vector<ushort>.Count;
 
 		ushort[] maxValues = new ushort[Vector<ushort>.Count];
-		for (int j = 0; j<Vector<ushort>.Count; j++)
+		for (int j = 0; j < Vector<ushort>.Count; j++)
 		{
-			maxValues[j]=255;
+			maxValues[j] = 255;
 		}
-		Vector<ushort> maxVector = new Vector<ushort>(maxValues, 0); 
-		await targetTask;		
+		Vector<ushort> maxVector = new Vector<ushort>(maxValues, 0);
+		await targetTask;
 		await fromTask;
-		for (int i = 0; i<length-remaining; i += Vector<ushort>.Count)
+		for (int i = 0; i < length - remaining; i += Vector<ushort>.Count)
 		{
 			var targetVector = new Vector<ushort>(targetTask.Result, i);
 			var fromVector = new Vector<ushort>(fromTask.Result, i);
 			ushort[] s = new ushort[Vector<ushort>.Count];
-			((targetVector*fromVector)/maxVector).CopyTo(s, 0);
-			for (int j = 0; j<Vector<ushort>.Count; j++)
+			((targetVector * fromVector) / maxVector).CopyTo(s, 0);
+			for (int j = 0; j < Vector<ushort>.Count; j++)
 			{
-				target[i+j]=(byte)s[j];
+				target[i + j] = (byte)s[j];
 			}
 		}
-		for (int i=length-remaining; i<length; i++){
-			target[i] = (byte)((target[i]*fromTask.Result[i])/255);
+		//GD.Print("fromtask: ", fromTask.Result.Length);
+		//GD.Print("target: ", length);
+		for (int i = length - remaining; i < length; i++)
+		{
+			target[i] = (byte)((target[i] * fromTask.Result[i]) / 255);
 		}
 		var elapsed = sw.ElapsedMilliseconds;
+
+
+		//GD.Print("combine done");
+		//var img = ImageTexture.CreateFromImage(Image.CreateFromData(256, 256, false, Image.Format.R8, target[0..65536]));
+		//sprite.Texture = img;
 	}
 
-	public void Refresh(){
+	public void Refresh()
+	{
 		if (Engine.IsEditorHint()) MeshGen();
 	}
 
-	public async void MeshGen(){
-		var verticesTask = new Task<Vector3[]>(()=>{
-			var vertices = new Vector3[(Subdivisions.X+2)*(Subdivisions.Y+2)];
-			for (int y = 0; y<Subdivisions.Y+2; y++){
-				for (int x = 0; x<Subdivisions.X+2; x++){
-					int i = y*(Subdivisions.X+2)+x;
-					vertices[i] = new Vector3(((x+((y%2==1)?0.25f:-0.25f))/(Subdivisions.X+1)-0.5f)*PlaneScale.X, 0, (((float)y)/((float)(Subdivisions.Y+1))-0.5f)*PlaneScale.Y);
+	public async void MeshGen()
+	{
+		var verticesTask = new Task<Vector3[]>(() =>
+		{
+			var vertices = new Vector3[(Subdivisions.X + 2) * (Subdivisions.Y + 2)];
+			for (int y = 0; y < Subdivisions.Y + 2; y++)
+			{
+				for (int x = 0; x < Subdivisions.X + 2; x++)
+				{
+					int i = y * (Subdivisions.X + 2) + x;
+					vertices[i] = new Vector3(((x + ((y % 2 == 1) ? 0.25f : -0.25f)) / (Subdivisions.X + 1) - 0.5f) * PlaneScale.X, 0, (((float)y) / ((float)(Subdivisions.Y + 1)) - 0.5f) * PlaneScale.Y);
 				}
 			}
 			return vertices;
 		});
 		verticesTask.Start();
-		var indicesTask = new Task<int[]>(()=>{
-			var indices = new int[(Subdivisions.X+1)*(Subdivisions.Y+1)*2*3];
-			for (int y = 0; y<Subdivisions.Y+1; y++){
-				for (int x = 0; x<Subdivisions.X+1; x++){
-					int i = (y*(Subdivisions.X+1)+x)*6;
-					if(y%2==0)
-					{	
-						indices[i]=y*(Subdivisions.X+2)+x;
-						indices[i+1]=y*(Subdivisions.X+2)+x+1;
-						indices[i+2]=(y+1)*(Subdivisions.X+2)+x;
-						indices[i+3]=(y+1)*(Subdivisions.X+2)+x;
-						indices[i+4]=y*(Subdivisions.X+2)+x+1;
-						indices[i+5]=(y+1)*(Subdivisions.X+2)+x+1;
-					} 
-					else 
+
+		var uvTask = new Task<Vector2[]>(() =>
+		{
+			var uv = new Vector2[(Subdivisions.X + 2) * (Subdivisions.Y + 2)];
+			for (int y = 0; y < Subdivisions.Y + 2; y++)
+			{
+				for (int x = 0; x < Subdivisions.X + 2; x++)
+				{
+					int i = y * (Subdivisions.X + 2) + x;
+					uv[i] = new Vector2((float)x / (float)Subdivisions.X + (float)1, (float)y / (float)Subdivisions.Y + (float)1);
+				}
+			}
+			return uv;
+		});
+		uvTask.Start();
+		var indicesTask = new Task<int[]>(() =>
+		{
+			var indices = new int[(Subdivisions.X + 1) * (Subdivisions.Y + 1) * 2 * 3];
+			for (int y = 0; y < Subdivisions.Y + 1; y++)
+			{
+				for (int x = 0; x < Subdivisions.X + 1; x++)
+				{
+					int i = (y * (Subdivisions.X + 1) + x) * 6;
+					if (y % 2 == 0)
 					{
-						indices[i]=y*(Subdivisions.X+2)+x;
-						indices[i+1]=(y+1)*(Subdivisions.X+2)+x+1;
-						indices[i+2]=(y+1)*(Subdivisions.X+2)+x;
-						indices[i+3]=y*(Subdivisions.X+2)+x;
-						indices[i+4]=y*(Subdivisions.X+2)+x+1;
-						indices[i+5]=(y+1)*(Subdivisions.X+2)+x+1;
+						indices[i] = y * (Subdivisions.X + 2) + x;
+						indices[i + 1] = y * (Subdivisions.X + 2) + x + 1;
+						indices[i + 2] = (y + 1) * (Subdivisions.X + 2) + x;
+						indices[i + 3] = (y + 1) * (Subdivisions.X + 2) + x;
+						indices[i + 4] = y * (Subdivisions.X + 2) + x + 1;
+						indices[i + 5] = (y + 1) * (Subdivisions.X + 2) + x + 1;
+					}
+					else
+					{
+						indices[i] = y * (Subdivisions.X + 2) + x;
+						indices[i + 1] = (y + 1) * (Subdivisions.X + 2) + x + 1;
+						indices[i + 2] = (y + 1) * (Subdivisions.X + 2) + x;
+						indices[i + 3] = y * (Subdivisions.X + 2) + x;
+						indices[i + 4] = y * (Subdivisions.X + 2) + x + 1;
+						indices[i + 5] = (y + 1) * (Subdivisions.X + 2) + x + 1;
 					}
 				}
 			}
@@ -175,13 +229,15 @@ public partial class Fog : MeshInstance3D
 		});
 		indicesTask.Start();
 		var arrayMesh = new ArrayMesh();
-		Mesh=arrayMesh;
+		Mesh = arrayMesh;
 		var arrays = new Godot.Collections.Array();
 		arrays.Resize((int)Mesh.ArrayType.Max);
 		await verticesTask;
 		await indicesTask;
+		await uvTask;
 		arrays[(int)Mesh.ArrayType.Vertex] = verticesTask.Result;
 		arrays[(int)Mesh.ArrayType.Index] = indicesTask.Result;
+		arrays[(int)Mesh.ArrayType.TexUV] = uvTask.Result;
 		arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, arrays);
 		arrayMesh.SurfaceSetMaterial(0, FogMaterial);
 	}
---
+++ Scripts/NeuronActivator.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class NeuronActivator : Node
+{
+	[Export] public Fog fog;
+
+	[Export] public Godot.Collections.Array<Texture2D> textures {get; set; }
+
+	int i = 0;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+
+	public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("DebugClick")){
+			GD.Print("Applying Fog ",i);
+			fog.Recede(textures[i]);
+			i++;
+		}
+    }
+}
