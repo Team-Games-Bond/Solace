7bedb94f5e53f8062d4d1e9a96e4ac6b1854be53
fzha191 <fzha191@aucklanduni.ac.nz>
2024-10-07 02:36:50 +1300

    added new features to tutorial

--- Scripts/Sequence.cs
+++ Scripts/Sequence.cs
@@ -5,55 +5,55 @@ using System.Net.NetworkInformation;
 
 public partial class Sequence : Node
 {
-    [Export] public Godot.Collections.Array<Button> sequenceButtons { get; set; }
-    [Export] bool reusable = false;
+	[Export] public Godot.Collections.Array<Button> sequenceButtons { get; set; }
+	[Export] bool reusable = false;
 	[Export] bool isEnabled = true;
-    [Signal] public delegate void CompletedEventHandler();
-    [Signal] public delegate void CorrectEventHandler();
-    [Signal] public delegate void IncorrectEventHandler();
-    private int sequenceIndex = 0;
+	[Signal] public delegate void CompletedEventHandler();
+	[Signal] public delegate void CorrectEventHandler();
+	[Signal] public delegate void IncorrectEventHandler();
+	private int sequenceIndex = 0;
 	
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        foreach (var button in sequenceButtons)
-        {
-            button.ButtonPress += ButtonPressed;
-        }
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		foreach (var button in sequenceButtons)
+		{
+			button.ButtonPress += ButtonPressed;
+		}
+	}
 
-    public void ButtonPressed(Button button)
-    {
+	public void ButtonPressed(Button button)
+	{
 		GD.Print("Current Sequence:", sequenceIndex);
-        if(button == sequenceButtons[sequenceIndex] && isEnabled)
-        {
-            SequenceCorrect();
-            GD.Print("Correct");
-        } else 
-        {
-            SequenceIncorrect();
-            GD.Print("Incorrect");
-        }
-    }
+		if(button == sequenceButtons[sequenceIndex] && isEnabled)
+		{
+			SequenceCorrect();
+			GD.Print("Correct");
+		} else 
+		{
+			SequenceIncorrect();
+			GD.Print("Incorrect");
+		}
+	}
 
-    private void SequenceCorrect()
-    {
-        sequenceIndex += 1;
-        if(sequenceIndex >= sequenceButtons.Count) SequenceCompleted();
-        else EmitSignal(SignalName.Correct);
-    }
+	private void SequenceCorrect()
+	{
+		sequenceIndex += 1;
+		if(sequenceIndex >= sequenceButtons.Count) SequenceCompleted();
+		else EmitSignal(SignalName.Correct);
+	}
 
-    private void SequenceIncorrect()
-    {
-        sequenceIndex = 0;
-        EmitSignal(SignalName.Incorrect);
-    }
+	private void SequenceIncorrect()
+	{
+		sequenceIndex = 0;
+		EmitSignal(SignalName.Incorrect);
+	}
 
-    private void SequenceCompleted()
-    {
-        GD.Print("Completed!");
-        EmitSignal(SignalName.Completed);
-        if(reusable) sequenceIndex = 0;
+	private void SequenceCompleted()
+	{
+		GD.Print("Completed!");
+		EmitSignal(SignalName.Completed);
+		if(reusable) sequenceIndex = 0;
 		else isEnabled = false;
-    }
+	}
 }
