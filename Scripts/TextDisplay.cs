c89cca3f7ebe9bda82e7d640d130d48ca4767872
Melody Elwood <belwood4@gmail.com>
2024-10-17 20:29:48 +1300

    Added Text-Display and Zoom Change game objects

--- Scripts/TextDisplay.cs
+++ Scripts/TextDisplay.cs
@@ -3,13 +3,58 @@ using System;
 
 public partial class TextDisplay : Node
 {
+	[Export] public string textToDisplay;
+	[Export] public double timeToDisplay = 3;
+	[Export] public double timeToType = 0.5;
+	[Export] public string rgb = "000";
+	[ExportGroup("Setup Settings")]
+	[Export] public Area3D area;
+	[Export] public CanvasLayer canvas;
+	[Export] public RichTextLabel textLabel;
+	[Export] public Timer disapearTimer;
+	[Export] public Timer typingTimer;
+
+	[Export] public bool isActive = true;
+
+	[Signal]
+	public delegate void InteractionBeginEventHandler(CharacterController player);
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		textLabel.Text = "[center][color=#"+rgb+"]"+textToDisplay+"[/color][/center]";
+
+		canvas.Visible = false;
+		disapearTimer.WaitTime = timeToDisplay;
+		typingTimer.WaitTime = timeToType;
+
+		disapearTimer.Timeout += RemoveText;
+		area.BodyEntered += EnteredZone;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    public override void _Process(double delta)
+    {
+        if (!typingTimer.IsStopped())
+		{
+			textLabel.VisibleRatio = (float)(1-(typingTimer.TimeLeft/typingTimer.WaitTime));
+		} else textLabel.VisibleRatio = 1;
+    }
+
+    public void EnteredZone(Node3D body)
 	{
+		if(!isActive) return;
+
+		CharacterController player = (CharacterController)body;
+
+		canvas.Visible = true;
+		disapearTimer.Start();
+		typingTimer.Start();
+	}
+
+	public void RemoveText()
+	{
+		if(!isActive) return;
+
+		canvas.Visible = false;
+		disapearTimer.Stop();
 	}
 }
